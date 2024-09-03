2d18c493842b9e43102082fe6f18b8844c5f7796
fagunsaraswat <93783895+fagunsaraswat@users.noreply.github.com>
2024-09-03 08:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,6 @@ def predict_datapoint():
         results = predict_pipeline.predict(pred_df)
         return render_template('home.html', results=results[0])
     
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0')
-         
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
